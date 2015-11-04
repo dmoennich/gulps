a15b3ba97b2431435793d47118419a5e51af61b8
Daniel Moennich <daniel.moennich@gmail.com>
2015-11-04 11:59:50 +0100

    init commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,22 @@
+/**
+ * Created by daniel on 04.11.15.
+ */
+let gulp = require("gulp");
+let babelify = require("babelify");
+let browserify = require("browserify");
+let source = require("vinyl-source-stream");
+
+
+gulp.task("default", () => {
+
+    return browserify({
+        entries: ["./browser/js/main.js"],
+        debug: true // add source map
+    }).transform(babelify)
+    .bundle()
+    .pipe(source("main-bundle.js"))
+    .pipe(gulp.dest("./dist/js"));
+
+});
+
+
